9a9fb3e60fca877b7ded21722c5d2af6df256ccb
Aaron William Po <apo2@uwo.ca>
2024-11-21 19:52:46 -0500

    Fix activeScene shared_ptr ref in GameEngine::update

--- src/GameEngine/GameEngine.cpp
+++ src/GameEngine/GameEngine.cpp
@@ -177,12 +177,12 @@ void GameEngine::cleanup() {
 }
 
 void GameEngine::update() {
-  const std::shared_ptr<Scene> activeScene = m_scenes[m_currentSceneName];
+  const std::shared_ptr<Scene> &activeScene = m_scenes[m_currentSceneName];
   if (activeScene == nullptr) {
     return;
   }
 
-  m_scenes[m_currentSceneName]->update();
+  activeScene->update();
 }
 
 SDL_Renderer *GameEngine::getRenderer() const {
